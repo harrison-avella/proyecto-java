868cb8992681a9e5fcebf60fd2ab39385d003627
Harrison Avella <55506257+harriison@users.noreply.github.com>
2021-06-29 10:31:18 -0500

    ejecutando el proyecto

--- src/main/java/com/comit/bikerama/BikeramaApplication.java
+++ src/main/java/com/comit/bikerama/BikeramaApplication.java
@@ -8,6 +8,7 @@ public class BikeramaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BikeramaApplication.class, args);
+		System.out.println("Esto es un texto");
 	}
 
 }
