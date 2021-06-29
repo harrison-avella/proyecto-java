7dcb8e14038f118377d6a881011d64cbf7972e1a
Harrison Avella <55506257+harrison-avella@users.noreply.github.com>
2021-06-29 10:23:24 -0500

    primer commit

---
+++ src/main/java/com/comit/bikerama/BikeramaApplication.java
@@ -0,0 +1,13 @@
+package com.comit.bikerama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeramaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeramaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comit/bikerama/BikeramaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comit.bikerama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeramaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
